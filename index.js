88a97c4c5ba4c5ee24426f6507c5be59d63e1854
butlern-mn <109308507+butlern-mn@users.noreply.github.com>
2023-03-20 11:45:16 -0500

    Revision 13
    Fixed Submit buttons hover and buttons on Wellness Dashboard hover.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+console.log("Is running")
 
+$(document).ready(function(){
+  $(".dashboardButtons").hover(function(){
+    $(this).css("background-color", "#29a3fc");
+    }, function(){
+    $(this).css("background-color", "#8FCDFB");
+  });
+});
 
