6fe23260902af79ef2096298c5bec730049137f3
butlern-mn <109308507+butlern-mn@users.noreply.github.com>
2023-03-20 12:54:07 -0500

    Revision 14
    Got the modal to work!!!  For the most part.  Currently X to close works.  Need to see if I can get the Done button to close to work.  Will ask TA.  Adjusted a few font sizes, and got Back to Bootcamp words linked as well.  Will review with project team tonight.

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 console.log("Is running")
 
 $(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+
+$(document).ready(function(){
   $(".dashboardButtons").hover(function(){
     $(this).css("background-color", "#29a3fc");
     }, function(){
     $(this).css("background-color", "#8FCDFB");
   });
-});
+});  
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("mymodalButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
